18270c8fc423ff19bf1db8d17a37c67f29367788
Roman Pogorelov <rmk140987@gmail.com>
2019-08-29 12:33:32 +0300

    5.8. JavaDoc. Code convention[#174326]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,11 +1,21 @@
 package ru.job4j.calculator;
 
+/**
+ * Allows to do the basic arithmetic operation(+,/,*,-)
+ * @author roman.pogorelov
+ * @since 28.08.2019
+ * @version 1
+ */
 public class Calculator {
+    /**
+     * Main method for testing add, div, multiply and subtract methods.
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
         multiply(2, 1);
-        subtrack(10, 5);
+        subtract(10, 5);
     }
 
     public static void add(double first, double second) {
@@ -23,7 +33,7 @@ public class Calculator {
         System.out.println(first + "+" + second + " = " + result);
     }
 
-    public static void subtrack(double first, double second) {
+    public static void subtract(double first, double second) {
         double result = first - second;
         System.out.println(first + "+" + second + " = " + result);
     }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Roman Pogorelov (rmk140987@gmail.com)
+ * @version 1
+ * @since 29.08.2019
+ */
+
+package ru.job4j.calculator;
