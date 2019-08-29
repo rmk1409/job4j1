6aaa685ccee7881da93f0dbc5b882e34bdc3c960
Roman Pogorelov <rmk140987@gmail.com>
2019-08-29 19:22:24 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#174338] - simplified code 2.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,40 +4,10 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
 
-        for (int i = 1, length = data[0].length; i < length; i++) {
-            boolean check1stDiagonal = data[i - 1][i - 1] == data[i][i];
-            boolean check2ndDiagonal = data[length - 1][length - 1] == data[length - 1 - i][length - 1 - i];
-
-            if (!check1stDiagonal && !check2ndDiagonal) {
+        for (int i = 1, length = data.length; i < length; i++) {
+            if (data[0][0] != data[i][i] || data[0][length - 1] != data[i][length - 1 - i])
                 result = false;
-                break;
-            }
-        }
-
-        return result;
-    }
-
-    private boolean check1stDiagonal(boolean[][] data, boolean sample) {
-        boolean result = true;
-
-        for (int i = 0; i < data[0].length; i++) {
-            if (sample != data[i][i]) {
-                result = false;
-                break;
-            }
-        }
-
-        return result;
-    }
-
-    private boolean check2ndDiagonal(boolean[][] data, boolean sample) {
-        boolean result = true;
-
-        for (int i = 0, j = data[0].length - 1; i < data[0].length; i++, j--) {
-            if (sample != data[i][j]) {
-                result = false;
-                break;
-            }
+            break;
         }
 
         return result;
