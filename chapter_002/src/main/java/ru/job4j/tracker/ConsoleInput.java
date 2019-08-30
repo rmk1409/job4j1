9ac0cfa4f2c4a806c644c82e8a2b055b49881e82
Roman Pogorelov <rmk140987@gmail.com>
2019-08-30 18:34:12 +0300

    2. Создания Manifest для проекта Tracker[#174432]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * In/output system.
  * Created by roman.pogorelov on 30.08.2019
  */
-public class ConsoleInput implements Input{
+public class ConsoleInput implements Input {
     private Scanner in = new Scanner(System.in);
 
     public String ask(String question) {
