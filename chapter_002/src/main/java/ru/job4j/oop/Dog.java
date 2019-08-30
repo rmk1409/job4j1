f299f40db810180f352473d65ed3179217ef1ef5
Roman Pogorelov <rmk140987@gmail.com>
2019-08-30 09:51:35 +0300

    1.1 Котики обормотики.[#174452]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+/**
+ * Simple class
+ * Created by roman.pogorelov on 30.08.2019
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
