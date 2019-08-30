a161d025de1a7746ebbdf8f861e309ff697fbb83
Roman Pogorelov <rmk140987@gmail.com>
2019-08-30 11:11:05 +0300

    4. Расстояние между точками в трехмерном пространстве.[#174449]

--- chapter_001/src/main/java/ru/job4j/calculator/Point.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Point.java
@@ -3,14 +3,22 @@ package ru.job4j.calculator;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     /**
      * Calculate the distance between two points
+     *
      * @param another
      * @return distance
      */
@@ -18,9 +26,19 @@ public class Point {
         return Math.sqrt(Math.pow(another.x - this.x, 2) + Math.pow(another.y - this.y, 2));
     }
 
+    /**
+     * Calculate the distance between two 3D points
+     *
+     * @param another
+     * @return distance
+     */
+    public double distance3d(Point another) {
+        return Math.sqrt(Math.pow(another.x - this.x, 2) + Math.pow(another.y - this.y, 2) + Math.pow(another.z - this.z, 2));
+    }
+
     public static void main(String[] args) {
         Point point = new Point(0, 0);
-        Point another  = new Point(0, 0);
+        Point another = new Point(0, 0);
         System.out.println("result (0, 0) to (2, 0) " + point.distance(another));
         System.out.println(String.format("result (%d, %d) to (%d, %d) %.2f", 0, 1, 2, 3, new Point(0, 1).distance(new Point(2, 3))));
         System.out.println(String.format("result (%d, %d) to (%d, %d) %.2f", 0, 10, 2, 3, new Point(0, 10).distance(new Point(2, 3))));
--- chapter_001/src/test/java/ru/job4j/calculator/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/PointTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 
 import java.awt.*;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
 public class PointTest {
     @Test
     public void distance() {
@@ -12,5 +16,10 @@ public class PointTest {
         Assert.assertEquals(7.3, new Point(0, 10).distance(new Point(2, 3)), 0.1);
         Assert.assertEquals(20.1, new Point(0, 1).distance(new Point(20, 3)), 0.1);
         Assert.assertEquals(21.1, new Point(0, 10).distance(new Point(20, 3)), 0.1);
+    }
+
+    @Test
+    public void distance3d() {
+        assertThat(10.24, closeTo(new Point(7, 4, 3).distance3d(new Point(17, 6, 2)), 0.01));
     }
 }
