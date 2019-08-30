c1e64aaf243c3104761dbf39fa393a691ef2e8a9
Roman Pogorelov <rmk140987@gmail.com>
2019-08-30 10:14:08 +0300

    1.4. Переводчик Русский-Английский.[#174457]

---
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+/**
+ * Simple class
+ * Created by roman.pogorelov on 30.08.2019
+ */
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng;
+    }
+}
