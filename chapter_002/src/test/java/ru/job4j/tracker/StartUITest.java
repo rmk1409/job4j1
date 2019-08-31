8f845edbefb46a390d154866d12ecded0b3afc32
Roman Pogorelov <rmk140987@gmail.com>
2019-08-31 13:11:52 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#174431]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -9,38 +15,162 @@ import static org.junit.Assert.*;
  * Created by roman.pogorelov on 31.08.2019
  */
 public class StartUITest {
+    /**
+     * Menu string
+     */
+    private String menu = new StringJoiner(
+            System.lineSeparator())
+            .add("Меню.")
+            .add("0. Add new item")
+            .add("1. Show all items")
+            .add("2. Edit item")
+            .add("3. Delete item")
+            .add("4. Find item by Id")
+            .add("5. Find items by name")
+            .add("6. Exit Program")
+            .toString();
+
+    /**
+     * Item storage
+     */
+    private Tracker tracker;
+
+    /**
+     * Вывод в консоль по дефолту
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * Буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void beforeTest() {
+        tracker = new Tracker();
+        System.out.println("Starting test...");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void afterTest() {
+        System.setOut(this.stdout);
+        System.out.println("Ending test...");
+    }
+
+    @Test
+    public void showAllEmpty() {
+        StartUI ui = new StartUI(new StubInput(new String[]{"1", "6"}), tracker);
+        ui.init();
+        assertThat(this.out.toString(), is(
+                new StringJoiner(
+                        System.lineSeparator(), "",
+                        System.lineSeparator())
+                        .add(menu)
+                        .add("--------------- Показ всех заявок ---------------")
+                        .add("Заявок нет.")
+                        .add("---------------------------------")
+                        .add(menu)
+                        .toString()
+                )
+        );
+    }
+
+    @Test
+    public void showAllNotEmpty() {
+        Item first = new Item("alex", "Descr");
+        this.tracker.add(first);
+        Item second = new Item("alex", "Descr2");
+        this.tracker.add(second);
+        StartUI ui = new StartUI(new StubInput(new String[]{"1", "6"}), tracker);
+        ui.init();
+        assertThat(this.out.toString(), is(
+                new StringJoiner(
+                        System.lineSeparator(), "",
+                        System.lineSeparator())
+                        .add(menu)
+                        .add("--------------- Показ всех заявок ---------------")
+                        .add(String.format("%s %s %s.", first.getId(), first.getName(), first.getDescription()))
+                        .add(String.format("%s %s %s.", second.getId(), second.getName(), second.getDescription()))
+                        .add("---------------------------------")
+                        .add(menu)
+                        .toString()
+                )
+        );
+    }
+
+    @Test
+    public void findById() {
+        Item first = new Item("alex", "Descr");
+        this.tracker.add(first);
+        StartUI ui = new StartUI(new StubInput(new String[]{"4", first.getId(), "6"}), tracker);
+        ui.init();
+        assertThat(this.out.toString(), is(
+                new StringJoiner(
+                        System.lineSeparator(), "",
+                        System.lineSeparator())
+                        .add(menu)
+                        .add("------------------- Поиск заявки по id -----------------")
+                        .add(String.format("Заявка найдена - %s %s %s.", first.getId(), first.getName(), first.getDescription()))
+                        .add("---------------------------------")
+                        .add(menu)
+                        .toString()
+                )
+        );
+    }
+
+    @Test
+    public void findByName() {
+        Item first = new Item("alex", "Descr");
+        this.tracker.add(first);
+        Item second = new Item("alex", "Descr2");
+        this.tracker.add(second);
+        Item third = new Item("max", "Descr3");
+        this.tracker.add(third);
+        StartUI ui = new StartUI(new StubInput(new String[]{"5", first.getName(), "6"}), tracker);
+        ui.init();
+        assertThat(this.out.toString(), is(
+                new StringJoiner(
+                        System.lineSeparator(), "",
+                        System.lineSeparator())
+                        .add(menu)
+                        .add("------------------- Поиск заявок по имени -----------------")
+                        .add(String.format("%s %s %s.", first.getId(), first.getName(), first.getDescription()))
+                        .add(String.format("%s %s %s.", second.getId(), second.getName(), second.getDescription()))
+                        .add("---------------------------------")
+                        .add(menu)
+                        .toString()
+                )
+        );
+    }
 
     @Test
     public void createItem() {
-        Tracker tracker = new Tracker();
         StartUI ui = new StartUI(new StubInput(new String[]{"0", "alex", "decr1", "6"}), tracker);
         ui.init();
-        Item item = tracker.findByName("alex")[0];
+        Item item = this.tracker.findByName("alex")[0];
         assertThat("alex", is(item.getName()));
         assertThat("decr1", is(item.getDescription()));
     }
 
     @Test
     public void editItem() {
-        Tracker tracker = new Tracker();
         Item previous = new Item("test1", "Descr");
-        tracker.add(previous);
-        StartUI ui = new StartUI(new StubInput(new String[]{"2", previous.getId(), "alex", "decr1", "6"}), tracker);
+        this.tracker.add(previous);
+        StartUI ui = new StartUI(new StubInput(new String[]{"2", previous.getId(), "alex", "decr1", "6"}), this.tracker);
         ui.init();
-        Item another = tracker.findByName("alex")[0];
+        Item another = this.tracker.findByName("alex")[0];
         assertThat("alex", is(another.getName()));
         assertThat("decr1", is(another.getDescription()));
     }
 
     @Test
     public void deleteItem() {
-        Tracker tracker = new Tracker();
         Item item = new Item("test1", "Descr");
-        tracker.add(item);
-        StartUI ui = new StartUI(new StubInput(new String[]{"3", item.getId(), "6"}), tracker);
-        Item find = tracker.findById(item.getId());
+        this.tracker.add(item);
+        StartUI ui = new StartUI(new StubInput(new String[]{"3", item.getId(), "6"}), this.tracker);
+        Item find = this.tracker.findById(item.getId());
         assertThat(item, is(find));
         ui.init();
-        assertThat(null, is(tracker.findById(item.getId())));
+        assertThat(null, is(this.tracker.findById(item.getId())));
     }
 }
