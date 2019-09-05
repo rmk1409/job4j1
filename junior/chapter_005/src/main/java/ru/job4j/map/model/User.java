c7dad9a52474e185537f277ff2a1be43f25b5eed
Roman Pogorelov <rmk140987@gmail.com>
2019-09-05 18:30:37 +0300

    4. Переопределить только equals[#176742]

--- junior/chapter_005/src/main/java/ru/job4j/map/model/User.java
+++ junior/chapter_005/src/main/java/ru/job4j/map/model/User.java
@@ -42,9 +42,19 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 
     @Override
     public String toString() {
