cc680279d90b15084e9d443bdbf9906f8f9b37e8
Roman Pogorelov <rmk140987@gmail.com>
2019-09-05 13:33:08 +0300

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#176732]

---
+++ junior/chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
@@ -0,0 +1,26 @@
+package ru.job4j.list;
+
+/**
+ * Created by roman.pogorelov on 05.09.2019
+ */
+public class SimpleStack<T> {
+    private SimpleLinkedList<T> container = new SimpleLinkedList<>();
+
+    /**
+     * должен возвращать значение и удалять его из коллекции.
+     *
+     * @return верхний элемент
+     */
+    public T poll() {
+        return this.container.delete();
+    }
+
+    /**
+     * помещает значение в коллекцию.
+     *
+     * @param value для помещения в коллекцию
+     */
+    public void push(T value) {
+        this.container.add(value);
+    }
+}
