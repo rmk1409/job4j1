436543b4fca241b19f8c78a6445a0d13a751234b
Roman Pogorelov <rmk140987@gmail.com>
2019-09-05 18:15:35 +0300

    1. Создать модель User[#176739]

---
+++ junior/chapter_005/src/main/java/ru/job4j/map/model/User.java
@@ -0,0 +1,43 @@
+package ru.job4j.map.model;
+
+import java.util.Calendar;
+
+/**
+ * TODO Description
+ * Created by roman.pogorelov on 05.09.2019
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}
