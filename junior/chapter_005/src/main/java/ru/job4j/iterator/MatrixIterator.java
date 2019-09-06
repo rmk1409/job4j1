1d7805525868dde1e8e3e389394b096fb929176b
Roman Pogorelov <rmk140987@gmail.com>
2019-09-06 12:53:57 +0300

    5.1.1. Итератор для двухмерного массива int[][][#176753] - refactoring

--- junior/chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ junior/chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -12,28 +12,12 @@ public class MatrixIterator implements Iterator {
 
     public MatrixIterator(int[][] array) {
         this.array = array;
-        this.findNext();
-    }
-
-    private void findNext() {
-        boolean result = false;
-        for (int i = horizontal; i < array.length; i++) {
-            if (vertical < array[i].length) {
-                result = true;
-                this.horizontal = i;
-                break;
-            } else {
-                vertical = 0;
-            }
-        }
-        if (!result) {
-            this.horizontal = -1;
-        }
     }
 
     @Override
     public boolean hasNext() {
-        return this.horizontal != -1;
+        return this.horizontal < this.array.length
+                && this.vertical < this.array[horizontal].length;
     }
 
     @Override
@@ -42,7 +26,10 @@ public class MatrixIterator implements Iterator {
             throw new NoSuchElementException();
         }
         int result = this.array[this.horizontal][this.vertical++];
-        this.findNext();
+        if (this.vertical == this.array[horizontal].length) {
+            this.vertical = 0;
+            this.horizontal++;
+        }
         return result;
     }
 }
