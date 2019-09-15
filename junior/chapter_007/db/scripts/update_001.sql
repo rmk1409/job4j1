ee83bbf0832ece527d4a87b9c7152385521de465
Roman Pogorelov <rmk140987@gmail.com>
2019-09-15 12:26:59 +0300

    4.5. Travis CI и PostgreSQL[#176705] - fix table creating 2

--- junior/chapter_007/db/scripts/update_001.sql
+++ junior/chapter_007/db/scripts/update_001.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS item("
-                    + "    id serial primary key,"
-                    + "    name character varying(50) ,"
-                    + "    description character varying(50)"
-                    + ");
+CREATE TABLE IF NOT EXISTS item(
+                    id serial primary key,
+                    name character varying(50) ,
+                    description character varying(50)
+                    );
