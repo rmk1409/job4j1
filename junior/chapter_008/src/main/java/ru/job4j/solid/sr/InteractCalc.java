601d7020af954ee2038a2a5b3f5d298958657808
Roman Pogorelov <rmk140987@gmail.com>
2019-09-17 15:11:27 +0300

    1. Использую класса Calculator. Сделать класс InteractCalc.[#176787]

---
+++ junior/chapter_008/src/main/java/ru/job4j/solid/sr/InteractCalc.java
@@ -0,0 +1,75 @@
+package ru.job4j.solid.sr;
+
+import ru.job4j.calculator.Calculator;
+
+import java.util.Scanner;
+
+/**
+ * Используя класс Calculator.
+ * 1. Сделать класс InteractCalc.
+ * 2. В классе должен быть пользовательский ввод.
+ * 3. Повторный выбор операции и переиспользование предыдущего вычисления.
+ * 4. Проект должен следовать SRP.
+ * <p>
+ * Created by roman.pogorelov on 17.09.2019
+ */
+public class InteractCalc {
+    private Scanner scanner;
+
+    public InteractCalc() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public InteractCalc(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    /**
+     * Show all operations.
+     */
+    private void showMenu() {
+        System.out.println("1. Multiply");
+        System.out.println("2. Divide");
+        System.out.println("3. Add");
+        System.out.println("4. Subtract");
+    }
+
+    /**
+     * Get required user input.
+     *
+     * @param msg to show
+     * @return user input
+     */
+    private int getInput(String msg) {
+        System.out.print(msg);
+        return this.scanner.nextInt();
+    }
+
+    /**
+     * Calculate basic math.
+     */
+    public void run() {
+        this.showMenu();
+        var operation = this.getInput("Choose operation: ");
+        var first = this.getInput("Input 1st number: ");
+        var second = this.getInput("Input 2nd number: ");
+        switch (operation) {
+            case 1:
+                Calculator.multiply(first, second);
+                break;
+            case 2:
+                Calculator.div(first, second);
+                break;
+            case 3:
+                Calculator.add(first, second);
+                break;
+            case 4:
+                Calculator.subtract(first, second);
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        new InteractCalc().run();
+    }
+}
---
+++ junior/chapter_008/src/test/java/ru/job4j/solid/sr/InteractCalcTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.solid.sr;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * TODO Description
+ * Created by roman.pogorelov on 17.09.2019
+ */
+public class InteractCalcTest {
+
+    /**
+     * Вывод в консоль по дефолту
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * Буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void beforeTest() {
+        System.out.println("Starting test...");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void afterTest() {
+        System.setOut(this.stdout);
+        System.out.println("Ending test...");
+    }
+
+    @Test
+    public void run() {
+        Scanner scanner = new Scanner("1\n10\n5\n");
+        InteractCalc calc = new InteractCalc(scanner);
+        calc.run();
+        String expected = new StringJoiner(System.lineSeparator())
+                .add("1. Multiply")
+                .add("2. Divide")
+                .add("3. Add")
+                .add("4. Subtract")
+                .add("Choose operation: Input 1st number: Input 2nd number: 10.0 * 5.0 = 50.0").add("")
+                .toString();
+        assertThat(this.out.toString(), is(expected));
+    }
+}
