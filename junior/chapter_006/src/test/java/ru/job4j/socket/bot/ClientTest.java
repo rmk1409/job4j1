4a00cf29a57959050d5d99828459db03ad7c8b54
Roman Pogorelov <rmk140987@gmail.com>
2019-09-17 11:35:35 +0300

    1. Бот[#176780] - fixed test

--- junior/chapter_006/src/test/java/ru/job4j/socket/bot/ClientTest.java
+++ junior/chapter_006/src/test/java/ru/job4j/socket/bot/ClientTest.java
@@ -1,10 +1,13 @@
 package ru.job4j.socket.bot;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 
 import static org.hamcrest.core.Is.is;
@@ -17,11 +20,34 @@ import static org.mockito.Mockito.when;
  * Created by roman.pogorelov on 11.09.2019
  */
 public class ClientTest {
+
+
+    /**
+     * Вывод в консоль по дефолту
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * Буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void beforeTest() {
+        System.out.println("Starting test...");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void afterTest() {
+        System.setOut(this.stdout);
+        System.out.println("Ending test...");
+    }
+
     @Test
     public void something() throws IOException {
         Socket socket = mock(Socket.class);
 
-        ByteArrayInputStream in = new ByteArrayInputStream("".getBytes());
+        ByteArrayInputStream in = new ByteArrayInputStream("Hello, dear friend, I'm a oracle.".getBytes());
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         when(socket.getInputStream()).thenReturn(in);
@@ -30,6 +56,6 @@ public class ClientTest {
         Client client = new Client(socket);
         client.start();
 
-        assertThat(out.toString().trim(), is("Hell"));
+        assertThat(this.out.toString(), is("\tHello, dear friend, I'm a oracle." + System.lineSeparator()));
     }
 }
