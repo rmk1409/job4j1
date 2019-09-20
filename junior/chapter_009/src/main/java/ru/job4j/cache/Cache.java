7306b33603c60b8387100bbae75e848bafe4bb53
Roman Pogorelov <rmk140987@gmail.com>
2019-09-20 12:25:45 +0300

    Реализации кеша на SoftReference[#176816] - refactoring

--- junior/chapter_009/src/main/java/ru/job4j/cache/Cache.java
+++ junior/chapter_009/src/main/java/ru/job4j/cache/Cache.java
@@ -5,6 +5,7 @@ import java.lang.ref.SoftReference;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,14 +36,19 @@ public class Cache {
         if (cached != null && cached.get() != null) {
             result = cached.get();
         } else {
-            try {
-                List<String> lines = Files.readAllLines(Paths.get(Cache.class.getClassLoader().getResource(key).toURI()));
-                result = String.join(System.lineSeparator(), lines);
-                this.innerStorage.put(key, new SoftReference<>(result));
-            } catch (IOException | URISyntaxException e) {
-                logger.log(Level.ALL, e.getMessage(), e);
-            }
+            result = this.readFile(key);
+            this.innerStorage.put(key, new SoftReference<>(result));
         }
         return result;
+    }
+
+    private String readFile(String fileName) {
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(Paths.get(Cache.class.getClassLoader().getResource(fileName).toURI()));
+        } catch (IOException | URISyntaxException e) {
+            logger.log(Level.ALL, e.getMessage(), e);
+        }
+        return String.join(System.lineSeparator(), lines);
     }
 }
